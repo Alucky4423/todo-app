33be2d0c45fc5a4f8ac536c0fbeaa830a750d35f
Alucky4423 <a.lucky2510@gmail.com>
2017-07-25 19:42:49 +0900

    update : toggle method 追加

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -1,7 +1,13 @@
 export default class Todo {
-  constructor(content, isCompleted = false) {
+  constructor(content) {
     this.id = new Date().getTime()
     this.content = content
-    this.isCompleted = isCompleted
+    this.isCompleted = false
+  }
+
+  static toggle(todo) {
+    const copied = Object.assign({}, todo)
+    copied.isCompleted = !copied.isCompleted
+    return copied
   }
 }
