0378f490f662afd0f1c1627f72563bb5112adf1a
Alucky4423 <a.lucky2510@gmail.com>
2017-07-25 18:59:41 +0900

    update : ブラウザにデータを保存するようにした

--- src/app/TodoApp.jsx
+++ src/app/TodoApp.jsx
@@ -4,18 +4,27 @@ import TodoList from './TodoList';
 import Todo from './Todo.js';
 
 class TodoApp extends React.Component {
+
+  static get SERIALIZE_KEY() {
+    return 'todoList'
+  }
+
   constructor() {
     super()
-    this.state = { todoList: [] }
+
+    const jsonTodoList = localStorage.getItem(TodoApp.SERIALIZE_KEY)
+    this.state = { todoList: (jsonTodoList) ? JSON.parse(jsonTodoList) : [] }
   }
 
   addTodo(todo) {
-    console.log(todo)
+    console.log("TodoApp#addTodo : " + todo)
     if (!todo) return
 
     this.setState({
       todoList: this.state.todoList.concat([ new Todo(todo) ]),
     })
+
+    localStorage.setItem(TodoApp.SERIALIZE_KEY, JSON.stringify(this.state.todoList))
   }
 
   toggleTodo(id) {
@@ -42,7 +51,7 @@ class TodoApp extends React.Component {
       <div id="Todo">
         <h1>TodoApp</h1>
         <TodoForm onClick={this.addTodo.bind(this)} />
-        <TodoList todoList={this.state.todos}
+        <TodoList todoList={this.state.todoList}
           onChange={this.toggleTodo.bind(this)}
           onClick={this.removeTodo.bind(this)} />
       </div>
