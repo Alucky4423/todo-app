b8e49bf6fea909382e318e3d0683db0c182fe228
Alucky4423 <a.lucky2510@gmail.com>
2017-07-25 18:11:21 +0900

    replace : TodoApp 関連ファイルを移動

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoApp from './TodoApp';
+import TodoApp from './app/TodoApp';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<TodoApp />, document.getElementById('root'));
