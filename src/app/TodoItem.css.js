a1599e235bb9420ae0c2bdf49affc21c4f503cb7
Alucky4423 <a.lucky2510@gmail.com>
2017-07-30 23:45:53 +0900

    add : todo-item のcss を追加

---
+++ src/app/TodoItem.css.js
@@ -0,0 +1,23 @@
+export default {
+  tr: {
+    height: 35,
+  },
+  td: {
+    checkbox: {},
+    content: {},
+    btn: {},
+  },
+  content: {
+    complete: {
+      textDecoration: "line-through",
+    },
+  },
+  deleteBtn: {
+    height: 35,
+    padding: '0 20px',
+    color: 'white',
+    backgroundColor: 'red',
+    borderStyle: 'none',
+    borderRadius: '10px',
+  },
+}
